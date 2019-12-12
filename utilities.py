822cd8cc150858851d240ff23b28b8c06192dd38
Jose Enmanuel Estrella <enmanuelgit@gmail.com>
2019-12-12 00:43:49 -0400

    adding modifications to the utilities.py file, comments and change the names of the variables so that they have more relationship, adding new methods

--- utilities.py
+++ utilities.py
@@ -1,26 +1,28 @@
+# utilities class, where all the methods to be reused in the project are found
+
 class Utilities():
 
-    # Constructor
+    # Constructor Method
     def __init__(self):
-        self.Lpar = [chr(9633), chr(9632), chr(9633), chr(
+        self.Lpair   = [chr(9633), chr(9632), chr(9633), chr(
             9632), chr(9633), chr(9632), chr(9633), chr(9632)]
-        self.Limp = [chr(9632), chr(9633), chr(9632), chr(
+        self.Lodd    = [chr(9632), chr(9633), chr(9632), chr(
             9633), chr(9632), chr(9633), chr(9632), chr(9633)]
-        self.Tablero = []
-        self.FBlack = ['', chr(9819), chr(9823)]
-        self.FWhite = ['', chr(9813), chr(9817)]
-        self.columna = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-        self.fila = [8, 7, 6, 5, 4, 3, 2, 1, 0]
+        self.board   = []
+        self.FBlack  = ['', chr(9819), chr(9823)]
+        self.FWhite  = ['', chr(9813), chr(9817)]
+        self.column  = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        self.row     = [8, 7, 6, 5, 4, 3, 2, 1, 0]
 
         
-    #metodo par
-    def par(self, n):
+    # Pair Method
+    def pair(self, n):
         if n % 2 == 0:
             return True
         return False
 
-    # imprimir tablero
-    def Print_Board(self,t,LB,LW):
+    # Print Board Method
+    def Print_Board(self,board,LB,LW):
 
         print()
         for LisN in LB:
@@ -29,7 +31,7 @@ class Utilities():
         print("_________________")
         print()
 
-        for items in t:
+        for items in board:
             for i in items:
                 print(i, end=' ')
             print()
@@ -39,113 +41,237 @@ class Utilities():
             print(LisN, end=' ')
         print()
 
-    # Buscar esa pieza en el tablero sin piezas
-    def piezaTablero(self, p,t):
-        return t[p[0]][p[1]]
+    # Find that piece on the board without pieces
+    def Piece_in_Board(self, p,board):
+        return board[p[0]][p[1]]
 
-    # Buscar esa pieza en el tablero pero con las fichas colocadas
-    def piezaAjedrez(self, p, t):
-        return t[p[0]][p[1]]
+    # Find that chess piece on the board but with the chips placed
+    def Piece_of_Chess(self, p, board):
+        return board[p[0]][p[1]]
 
-#validacion al mover la ficha
-    def ValidarFichas(self,f1,f2,tablero,ColorF):
-        filaAct  = self.fila.index(f1[0])
-        columnaAct =self.columna.index(f1[1])
-        filaDest= self.fila.index(f2[0])
-        columnaDest =self.columna.index(f2[1])
-        if tablero[filaAct][columnaAct] not in ColorF :
-            print("No es su ficha")
+    # validation when moving the piece if there is a piece of your equipment where it goes
+    def Validate_Piece(self,posAct,PosDest,board,ColorPiece):
+        rowAct      = self.row.index(posAct[0])
+        columnAct   = self.column.index(posAct[1])
+        rowDest     = self.row.index(PosDest[0])
+        columnDest  = self.column.index(PosDest[1])
+
+        if board[rowAct][columnAct] not in ColorPiece :
+            print("not your piece")
             return False
-        elif  tablero[filaDest][columnaDest] in ColorF:
-            print("no puede moverse porque hay una ficha de su equipo en esa posicion")
+        elif  board[rowDest][columnDest] in ColorPiece:
+            print("there is a piece of your team in that position")
             return False
         return True  
 
 
-# recorriendo coordenadas por donde pasara la ficha
-    def cicloRecorrido(self,f1,f2,colorF,tablero):
-        filaAct  = self.fila.index(f1[0])
-        filaDest= self.fila.index(f2[0])
-        columnaDest =self.columna.index(f2[1])
-        for items in range(int(filaDest),int(filaAct)):
-            if tablero[items][columnaDest] in colorF:
-                print("No puede volar ficha de su equipo")
+    #  crossing coordinates where the piece will pass
+    def Cycle_Travel(self,posAct,PosDest,ColorPiece,board):
+        rowAct      = self.row.index(posAct[0])
+        rowDest     = self.row.index(PosDest[0])
+        columnDest  = self.column.index(PosDest[1])
+
+        for items in range(int(rowDest),int(rowAct)):
+            if board[items][columnDest] in ColorPiece:
+                print("can't fly piece of your team")
                 return False
         return True 
 
-    #Validar movimiento de la reina en diagonal secundaria
-    def ValidarReinaDiagonalSecundaria(self,f1,f2,colorF,tablero):
-        filaAct  = self.fila.index(f1[0])
-        filaDest= self.fila.index(f2[0])
-        columnaDest =self.columna.index(f2[1])
-        contC = columnaDest
-        if filaDest < filaAct:#para que tambien valide al moverse para atras
-            for f in range(int(filaDest),int(filaAct)):
-                if tablero[f][contC] in colorF:
-                    print("No puede volar ficha de su equipo")
-                    return False
+    # Validate movement of the secondary diagonal queen
+    def Validate_Queen_Diagonal_second(self,PosAct,PosDest,ColorPiece,ColorPieceEne,board):
+        rowAct      = self.row.index(PosAct[0])
+        columnAct   =self.column.index(PosAct[1])
+        rowDest     = self.row.index(PosDest[0])
+        columnDest  = self.column.index(PosDest[1])
+        contC       = columnDest
+        contAct     = columnAct - 1 
+        TemRecorDest = rowDest 
+
+        if rowDest < rowAct:# so that it also validates when moving backwards
+            for f in range(int(rowDest),int(rowAct)):
+                if board[f][contC] in ColorPiece :
+                    print("can't fly piece of your team")
+                    return False 
+
+                if f != TemRecorDest  and  board[f][contC] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False     
                 contC -= 1
             return True  
         else:
-            for f in range(int(filaAct+1),int(filaDest+1)):
-                if tablero[f][contC] in colorF:
-                    print("No puede volar ficha de su equipo")
+            for f in range(int(rowAct+1),int(rowDest+1)):
+                if board[f][contAct] in ColorPiece:
+                    print("can't fly piece of your team")
                     return False
+
+                if f != rowDest+1 and  board[f][contAct] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False 
+                contAct -= 1
+            return True
+
+
+    # validate that the queen moves in the secondary diagonal
+    def Validate_Queen_Diagonal_second_Black(self,PosAct,PosDest,ColorPiece,ColorPieceEne,board):
+        if ColorPiece == self.FWhite:#validate that the queen is white
+            return self.Validate_Queen_Diagonal_second(PosAct,PosDest,ColorPiece,ColorPieceEne,board)  
+
+        rowAct      = self.row.index(PosAct[0])
+        columnAct   = self.column.index(PosAct[1])
+        rowDest     = self.row.index(PosDest[0])
+        columnDest  = self.column.index(PosDest[1])
+
+        TemRecorDest = rowDest
+
+        if rowDest < rowAct:# so that it also validates when moving backwards
+            contC = columnDest
+            for f in range(int(rowDest),int(rowAct)):
+                if board[f][contC] in ColorPiece:
+                    print("can't fly piece of your team")
+                    return False
+
+                if f != TemRecorDest  and  board[f][contC] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False     
+                contC -= 1
+            return True  
+        else:
+            contC = columnAct - 1
+            for f in range(int(rowAct+1),int(rowDest+1)):
+                if board[f][contC] in ColorPiece:
+                    print("can't fly piece of your team")
+                    return False
+
+                if f != rowDest and  board[f][contC] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False    
                 contC -= 1
             return True
 
-    #Validar movimiento de la reina en diagonal principal
-    def ValidarReinaDiagonalPrincipal(self,f1,f2,colorF,tablero):
-        filaAct  = self.fila.index(f1[0])
-        filaDest= self.fila.index(f2[0])
-        columnaDest =self.columna.index(f2[1])
-        contC = columnaDest
-        if filaDest < filaAct:#para que tambien valide al moverse para atras
-            for f in range(int(filaDest),int(filaAct)):
-                if tablero[f][contC] in colorF:
-                    print("No puede volar ficha de su equipo")
+    # validate that the queen moves on the main diagonal
+    def Validate_Queen_Diagonal_Principal(self,PosAct,PosDest,ColorPiece,ColorPieceEne,board):
+        rowAct      = self.row.index(PosAct[0])
+        columnAct   = self.column.index(PosAct[1])
+        rowDest     = self.row.index(PosDest[0])
+        columnDest  = self.column.index(PosDest[1])
+
+        contAct     = columnAct + 1 
+        TemRecorDest = rowDest
+        contC = columnDest
+        if rowDest < rowAct:# so that it also validates when moving backwards
+            for f in range(int(rowDest),int(rowAct)):
+                if board[f][contC] in ColorPiece:
+                    print("can't fly piece of your team")
                     return False
+
+                if f != TemRecorDest  and  board[f][contC] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False     
                 contC += 1
             return True 
         else:
-            for f in range(int(filaAct+1),int(filaDest+1)):
-                if tablero[f][contC] in colorF:
-                    print("No puede volar ficha de su equipo")
+            for f in range(int(rowAct+1),int(rowDest+1)):
+                if board[f][contAct] in ColorPiece:
+                    print("can't fly piece of your team")
                     return False
+
+                if f != rowDest+1 and  board[f][contAct] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False    
+                contAct += 1
+            return True 
+
+    # validate that the queen moves on the main diagonal
+    def Validate_Queen_Diagonal_Principal_Black(self,PosAct,PosDest,ColorPiece,ColorPieceEne,board):
+        if ColorPiece == self.FWhite:#validate that the queen is white
+            return self.Validate_Queen_Diagonal_Principal(PosAct,PosDest,ColorPiece,ColorPieceEne,board) 
+
+        rowAct      = self.row.index(PosAct[0])
+        columnAct   = self.column.index(PosAct[1])
+        rowDest     = self.row.index(PosDest[0])
+        columnDest  = self.column.index(PosDest[1])
+        TemRecorDest = rowDest
+
+        if rowDest < rowAct:# so that it also validates when moving backwards
+            contC = columnDest
+            for f in range(int(rowDest),int(rowAct)):
+                if board[f][contC] in ColorPiece:
+                    print("can't fly piece of your team")
+                    return False
+
+                if f != TemRecorDest  and  board[f][contC] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False    
+                contC += 1
+            return True 
+        else:
+            contC = columnAct + 1
+            for f in range(int(rowAct+1),int(rowDest+1)):
+                if board[f][contC] in ColorPiece:
+                    print("can't fly piece of your team")
+                    return False
+
+                if f != rowDest and  board[f][contC] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False    
                 contC += 1
             return True 
 
-    #Validar movimiento de la reina en vertical
-    def ValidarReinaVertical(self,f1,f2,colorF,tablero):
-        filaAct  = self.fila.index(f1[0])
-        filaDest= self.fila.index(f2[0])
-        columnaDest =self.columna.index(f2[1])
-        if filaDest < filaAct:#para que tambien valide al moverse para atras
-            for f in range(int(filaDest),int(filaAct)):
-                if tablero[f][columnaDest] in colorF:
-                    print("No puede volar ficha de su equipo")
+    #  Validate the queen's movement vertically
+    def Validate_Queen_Vertical(self,PosAct,PosDest,ColorPiece,ColorPieceEne,board):
+        rowAct      = self.row.index(PosAct[0])
+        rowDest     = self.row.index(PosDest[0])
+        columnDest  = self.column.index(PosDest[1])
+
+        if rowDest < rowAct:# so that it also validates when moving backwards
+            for f in range(int(rowDest),int(rowAct)):
+                print(f,columnDest,board[f][columnDest])
+                if board[f][columnDest] in ColorPiece:
+                    print("can't fly piece of your team")
                     return False
+
+                if f!= rowDest and board[f][columnDest] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False     
             return True 
         else:
-            for f in range(int(filaAct+1),int(filaDest+1)):
-                if tablero[f][columnaDest] in colorF:
-                    print("No puede volar ficha de su equipo")
+            for f in range(int(rowAct+1),int(rowDest+1)):
+                print(f,columnDest,board[f][columnDest])
+                if board[f][columnDest] in ColorPiece:
+                    print("can't fly piece of your team")
                     return False
+
+                if f!= rowDest+1 and board[f][columnDest] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False    
             return True
 
-    #Validar movimiento de la reina en orizontal
-    def ValidarReinaOrizontal(self,f1,f2,colorF,tablero):
-        filaAct  = self.fila.index(f1[0])
-        columnaDest =self.columna.index(f2[1])
-        if filaAct < columnaDest:#para que tambien valide al moverse para atras
-            for f in range(int(filaAct+1),int(columnaDest+1)):
-                if tablero[filaAct][f] in colorF:
-                    print("No puede volar ficha1")
+    # Validate the queen's movement horizontally
+    def Validate_Queen_Horizontal(self,PosAct,PosDest,ColorPiece,ColorPieceEne,board):
+        rowAct      = self.row.index(PosAct[0])
+        columnAct   = self.column.index(PosAct[1])
+        columnDest  = self.column.index(PosDest[1])
+
+        if columnAct < columnDest:# so that it also validates when moving backwards
+            for f in range(int(columnAct+1),int(columnDest+1)):
+                if board[rowAct][f] in ColorPiece:
+                    print("can't fly piece of your team")
                     return False
+
+                if f!= columnDest+1 and board[rowAct][f] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False       
             return True  
         else:
-            for f in range(int(columnaDest),int(filaAct)):
-                if tablero[filaAct][f] in colorF:
-                    print("No puede volar ficha2")
+            for f in range(int(columnDest),int(columnAct)):
+                if board[rowAct][f] in ColorPiece:
+                    print("can't fly piece of your team")
                     return False
-            return True           
+
+                if f!= columnDest and board[rowAct][f] in ColorPieceEne:
+                    print("can't fly piece of your team contrary")
+                    return False     
+            return True     
+
+    
+
