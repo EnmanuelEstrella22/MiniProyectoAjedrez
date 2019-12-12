d1c0bff3698436cf1f9879dee429f31c89f4052b
Jose Enmanuel Estrella <enmanuelgit@gmail.com>
2019-12-12 00:41:19 -0400

    adding modifications to the piece.py file, comments and change the names of the variables so that they have more relationship

--- piece.py
+++ piece.py
@@ -1,76 +1,84 @@
 from board import *
 
-# metodos de la reina y el peon
+# class piece where the pawn and queen's methods are located. this class inherits from the chess board
+
+# in this is the pawn method with each of the validations of its movements
+# the pawn method will also validate a white pawn like a black pawn
+
+# also the queen's method meets each of the validations of her movements
+# the queen's method will validate a white queen like a black queen
+
 class Piece(Chessboard):
 
-    #metodo para establecer las reglas y los movimientos del peon
-    def Peon(self,posActual,AdondeVa,tablero,ColorFicha,ColorFichaEne,num1,num2,num3):
-        fiA = self.fila.index(posActual[0])
-        coA = self.columna.index(posActual[1])
-        fiVa = self.fila.index(AdondeVa[0])
-        coVa = self.columna.index(AdondeVa[1])
-        listado = (tablero[self.fila.index(AdondeVa[0])] [self.columna.index(AdondeVa[1])])
-        if AdondeVa[1] != posActual[1]:
-            if fiA + coA == fiVa + coVa or fiA - coA == fiVa - coVa and abs(self.fila.index(AdondeVa[0]) - self.fila.index(posActual[0])) == 1:
-                if self.ValidarFichas(posActual,AdondeVa,tablero,ColorFicha):
-                    if tablero[self.fila.index(AdondeVa[0])] [self.columna.index(AdondeVa[1])] in ColorFichaEne:
-                         print("comio ficha") 
-                         return [self.Piece_Move(tablero,posActual,AdondeVa),listado]
+    # method to establish the rules and movements of the pawn
+    def Pawn(self,posAct,PosDest,board,ColorPiece,ColorPieceEne,num1,num2,num3):
+        RowAct = self.row.index(posAct[0])
+        ColumnAct = self.column.index(posAct[1])
+        RowDest = self.row.index(PosDest[0])
+        ColumnDest = self.column.index(PosDest[1])
+        ListPiece = (board[self.row.index(PosDest[0])] [self.column.index(PosDest[1])])
+        if PosDest[1] != posAct[1]:
+            if RowAct + ColumnAct == RowDest + ColumnDest or RowAct - ColumnAct == RowDest - ColumnDest and abs(self.row.index(PosDest[0]) - self.row.index(posAct[0])) == 1:
+                if self.Validate_Piece(posAct,PosDest,board,ColorPiece):
+                    if board[self.row.index(PosDest[0])] [self.column.index(PosDest[1])] in ColorPieceEne:
+                         print("Ate Piece") 
+                         return [self.Piece_Move(board,posAct,PosDest),ListPiece]
                     else:
-                        print("No tiene ficha para comer")
+                        print("don't has piece for ate")
+                else:
+                    print("the pawn can't fly piece")         
             else:  
-                print("El Peon solo se mueve verticalmente")
-        elif self.fila.index(posActual[0]) == num1 :
-            if self.fila.index(posActual[0])+self.fila.index(AdondeVa[0]) > num2 and self.fila.index(posActual[0])+self.fila.index(AdondeVa[0]) < num3 and  tablero[self.fila.index(AdondeVa[0])] [self.columna.index(AdondeVa[1])] not in ColorFichaEne:  
-                if self.cicloRecorrido(posActual,AdondeVa,ColorFichaEne,tablero):
-                    return [self.Piece_Move(tablero,posActual,AdondeVa),""]      
+                print("The Pawn only moves vertically")
+        elif self.row.index(posAct[0]) == num1 :
+            if self.row.index(posAct[0])+self.row.index(PosDest[0]) > num2 and self.row.index(posAct[0])+self.row.index(PosDest[0]) < num3 and  board[self.row.index(PosDest[0])] [self.column.index(PosDest[1])] not in ColorPieceEne:  
+                if self.Cycle_Travel(posAct,PosDest,ColorPieceEne,board):
+                    return [self.Piece_Move(board,posAct,PosDest),""]      
             else: 
-                print("Movimiento invalido")
-        else:#validar que se mueva solo un paso a delante si ya salio de su posicion
-            if num1 != 6: #Si no es ficha blanca
-                if self.fila.index(AdondeVa[0]) > self.fila.index(posActual[0]) and self.fila.index(AdondeVa[0]) - self.fila.index(posActual[0]) == 1 and  tablero[self.fila.index(AdondeVa[0])] [self.columna.index(AdondeVa[1])] not in ColorFichaEne:       
-                    if self.ValidarFichas(posActual,AdondeVa,tablero,ColorFicha):
-                        return [self.Piece_Move(tablero,posActual,AdondeVa),""]
+                print("invalid movement")
+        else:# validate that it moves only one step forward if it has already left its position
+            if ColorPiece == self.FBlack: # If it is not white piece
+                if self.row.index(PosDest[0]) > self.row.index(posAct[0]) and self.row.index(PosDest[0]) - self.row.index(posAct[0]) == 1 and  board[self.row.index(PosDest[0])] [self.column.index(PosDest[1])] not in ColorPieceEne:       
+                    if self.Validate_Piece(posAct,PosDest,board,ColorPiece):
+                        return [self.Piece_Move(board,posAct,PosDest),""]
                 else: 
-                    print("Movimiento invalido")
+                    print("invalid movement")
             else: 
-                if self.fila.index(AdondeVa[0]) < self.fila.index(posActual[0]) and abs(self.fila.index(AdondeVa[0]) - self.fila.index(posActual[0])) == 1 and  tablero[self.fila.index(AdondeVa[0])] [self.columna.index(AdondeVa[1])] not in ColorFichaEne:       
-                    if self.ValidarFichas(posActual,AdondeVa,tablero,ColorFicha):
-                        return [self.Piece_Move(tablero,posActual,AdondeVa),""]
+                if self.row.index(PosDest[0]) < self.row.index(posAct[0]) and abs(self.row.index(PosDest[0]) - self.row.index(posAct[0])) == 1 and  board[self.row.index(PosDest[0])] [self.column.index(PosDest[1])] not in ColorPieceEne:       
+                    if self.Validate_Piece(posAct,PosDest,board,ColorPiece):
+                        return [self.Piece_Move(board,posAct,PosDest),""]
                 else: 
-                    print("Movimiento invalido")   
+                    print("invalid movement")   
         return False 
 
 
-    #metodo para establecer las reglas y los movimientos de la reina
-    def Reina(self,posActual,AdondeVa,tablero,ColorFicha,ColoFichaE):
-
-        fiA = self.fila.index(posActual[0])
-        coA = self.columna.index(posActual[1])
-        fiVa = self.fila.index(AdondeVa[0])
-        coVa = self.columna.index(AdondeVa[1])
-        listado = (tablero[self.fila.index(AdondeVa[0])] [self.columna.index(AdondeVa[1])])
-        if fiA == fiVa  or coA == coVa   or fiA + coA == fiVa + coVa or fiA - coA == fiVa - coVa:      
-            if fiA + coA == fiVa + coVa and self.ValidarReinaDiagonalSecundaria(posActual,AdondeVa,ColorFicha,tablero):
-                 if tablero[self.fila.index(AdondeVa[0])] [self.columna.index(AdondeVa[1])] in ColoFichaE:
-                    print("comio ficha")
-                    return [self.Piece_Move(tablero,posActual,AdondeVa),listado]
-                 return [self.Piece_Move(tablero,posActual,AdondeVa),""]   
-            if fiA - coA == fiVa - coVa and self.ValidarReinaDiagonalPrincipal(posActual,AdondeVa,ColorFicha,tablero):
-                if tablero[self.fila.index(AdondeVa[0])] [self.columna.index(AdondeVa[1])] in ColoFichaE:
-                    print("comio ficha")
-                    return [self.Piece_Move(tablero,posActual,AdondeVa),listado]
-                return [self.Piece_Move(tablero,posActual,AdondeVa),""]     
-            if fiA == fiVa and self.ValidarReinaOrizontal(posActual,AdondeVa,ColorFicha,tablero):
-                if tablero[self.fila.index(AdondeVa[0])] [self.columna.index(AdondeVa[1])] in ColoFichaE:
-                    print("comio ficha")
-                    return [self.Piece_Move(tablero,posActual,AdondeVa),listado]
-                return [self.Piece_Move(tablero,posActual,AdondeVa),""]     
-            if coA == coVa and self.ValidarReinaVertical(posActual,AdondeVa,ColorFicha,tablero):
-                if tablero[self.fila.index(AdondeVa[0])] [self.columna.index(AdondeVa[1])] in ColoFichaE:
-                    print("comio ficha")
-                    return [self.Piece_Move(tablero,posActual,AdondeVa),listado]
-                return [self.Piece_Move(tablero,posActual,AdondeVa),""]     
+    # method to establish the rules and movements of the queen
+    def Queen(self,posAct,PosDest,board,ColorPiece,ColorPieceEne):
+        RowAct = self.row.index(posAct[0])
+        ColumnAct = self.column.index(posAct[1])
+        RowDest = self.row.index(PosDest[0])
+        ColumnDest = self.column.index(PosDest[1])
+        ListPiece = (board[self.row.index(PosDest[0])] [self.column.index(PosDest[1])])
+        if RowAct == RowDest  or ColumnAct == ColumnDest   or RowAct + ColumnAct == RowDest + ColumnDest or RowAct - ColumnAct == RowDest - ColumnDest:      
+            if RowAct + ColumnAct == RowDest + ColumnDest and self.Validate_Queen_Diagonal_second_Black(posAct,PosDest,ColorPiece,ColorPieceEne,board):
+                 if board[self.row.index(PosDest[0])] [self.column.index(PosDest[1])] in ColorPieceEne:
+                    print("Ate Piece")
+                    return [self.Piece_Move(board,posAct,PosDest),ListPiece]
+                 return [self.Piece_Move(board,posAct,PosDest),""]   
+            if RowAct - ColumnAct == RowDest - ColumnDest and self.Validate_Queen_Diagonal_Principal_Black(posAct,PosDest,ColorPiece,ColorPieceEne,board):
+                if board[self.row.index(PosDest[0])] [self.column.index(PosDest[1])] in ColorPieceEne:
+                    print("Ate Piece")
+                    return [self.Piece_Move(board,posAct,PosDest),ListPiece]
+                return [self.Piece_Move(board,posAct,PosDest),""]     
+            if RowAct == RowDest and self.Validate_Queen_Horizontal(posAct,PosDest,ColorPiece,ColorPieceEne,board):
+                if board[self.row.index(PosDest[0])] [self.column.index(PosDest[1])] in ColorPieceEne:
+                    print("Ate Piece")
+                    return [self.Piece_Move(board,posAct,PosDest),ListPiece]
+                return [self.Piece_Move(board,posAct,PosDest),""]     
+            if ColumnAct == ColumnDest and self.Validate_Queen_Vertical(posAct,PosDest,ColorPiece,ColorPieceEne,board):
+                if board[self.row.index(PosDest[0])] [self.column.index(PosDest[1])] in ColorPieceEne:
+                    print("Ate Piece")
+                    return [self.Piece_Move(board,posAct,PosDest),ListPiece]
+                return [self.Piece_Move(board,posAct,PosDest),""]     
         else:
-            print("Coordenada invalina")
+            print("invalid coordinate")
         return False
